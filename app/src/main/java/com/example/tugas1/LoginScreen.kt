c5017eb39e7d6b4adda00adfe8459308b71ff98b
Izalhadji31 <ryzalhadji@gmail.com>
2025-07-30 04:26:19 +0700

    Menambahkan Spacer dan text Login

--- app/src/main/java/com/example/tugas1/LoginScreen.kt
+++ app/src/main/java/com/example/tugas1/LoginScreen.kt
@@ -3,13 +3,19 @@ package com.example.tugas1
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun LoginScreen() {
@@ -20,5 +26,14 @@ fun LoginScreen() {
             .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Top
-    ) {}
+    ) {
+        Spacer(modifier = Modifier.height(40.dp))
+
+        Text(
+            text = "Login",
+            style = TextStyle(fontSize = 32.sp, fontWeight = FontWeight.Bold),
+            color = Color.Black
+        )
+
+    }
 }
