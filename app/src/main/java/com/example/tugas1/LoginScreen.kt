478160c2f6e3396dc3b662b741f70b38179377d0
Izalhadji31 <ryzalhadji@gmail.com>
2025-07-30 04:30:59 +0700

    Menambahkan Spacer dan Image untuk gambar

--- app/src/main/java/com/example/tugas1/LoginScreen.kt
+++ app/src/main/java/com/example/tugas1/LoginScreen.kt
@@ -1,5 +1,6 @@
 package com.example.tugas1
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -7,11 +8,13 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -40,7 +43,13 @@ fun LoginScreen() {
             text = "Halaman Login",
             style = TextStyle(fontSize = 16.sp, color = Color.Gray)
         )
+        Spacer(modifier = Modifier.height(5.dp))
 
+        Image(
+            painter = painterResource(id = R.drawable.logo_umy),
+            contentDescription = "Logo Universitas",
+            modifier = Modifier.size(300.dp)
+        )
 
     }
 }
