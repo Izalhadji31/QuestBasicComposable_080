7c7432b47f3d6d722fbf28c107126eebe4589497
Izalhadji31 <ryzalhadji@gmail.com>
2025-07-30 04:24:20 +0700

    Menambahkan LoginScreen ke dalam MainActivity.kt

--- app/src/main/java/com/example/tugas1/MainActivity.kt
+++ app/src/main/java/com/example/tugas1/MainActivity.kt
@@ -22,6 +22,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             TUGAS1Theme {
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    LoginScreen()
                 }
             }
         }
