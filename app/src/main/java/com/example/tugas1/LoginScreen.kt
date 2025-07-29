7e144bfd61dd1946924c8c936eb68162d5b62b47
Izalhadji31 <ryzalhadji@gmail.com>
2025-07-30 04:23:56 +0700

    Menambahkan LoginScreen.kt ke dalam example

---
+++ app/src/main/java/com/example/tugas1/LoginScreen.kt
@@ -0,0 +1,24 @@
+package com.example.tugas1
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun LoginScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {}
+}
